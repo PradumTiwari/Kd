55c5efb9f2de166f867ee98f2132ba024d2d1e5f
Pradum <satendarpradum@gmail.com>
2025-05-24 19:11:06 +0530

    Added Papa Photo

--- components/AnimatedTestimonialsDemo.tsx
+++ components/AnimatedTestimonialsDemo.tsx
@@ -5,9 +5,9 @@ export function AnimatedTestimonialsDemo() {
     {
       quote:
         "The attention to detail and innovative features have completely transformed our workflow. This is exactly what we've been looking for.",
-      name: "Fututre Owner",
+      name: " Owner",
       designation: "Product Manager at TechFlow",
-      src: "https://github.com/PradumTiwari/Kd/blob/master/Assets/445799879_1363958224246817_4559949347578493852_n.jpg",
+      src: "",
     },{
       quote:
         "The attention to detail and innovative features have completely transformed our workflow. This is exactly what we've been looking for.",
